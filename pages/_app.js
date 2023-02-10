1ea133f387e361655f865d3fe19a0087921aed52
Sett Teta <stetanonsakul@gmail.com>
2023-02-10 15:37:08 +0700

    added react-bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { SSRProvider } from 'react-bootstrap';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SSRProvider>
+      <Component {...pageProps} />
+    </SSRProvider>
+  )
+  
 }
