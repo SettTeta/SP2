da4589c028da35900a94c54f67cb345a31d93b38
Sett Teta <stetanonsakul@gmail.com>
2023-02-11 16:44:49 +0700

    all clear...

--- pages/Card.js
+++ pages/Card.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 function CardTemplate(props) {
-    const { link, desc, onDelete } = props
+    const { link, desc } = props
     return (
         <div className="col-md-4">
             <div className="card mb-4 box-shadow">
@@ -13,7 +13,7 @@ function CardTemplate(props) {
                     <div className="d-flex justify-content-between align-items-center">
                         <div className="btn-group">
                             <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                            <button type="button" className="btn btn-sm btn-outline-secondary" onClick={onDelete}>Del</button>
+                            <button type="button" className="btn btn-sm btn-outline-secondary">Del</button>
                         </div>
                         <small className="text-muted">9 mins</small>
                     </div>
