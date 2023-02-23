e67c4c927ec4d5069286723f47d97b2914d81a88
Sett Teta <stetanonsakul@gmail.com>
2023-02-23 12:44:53 +0700

    upadate 2

--- pages/api/browse/videos/[id].js
+++ pages/api/browse/videos/[id].js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
         const updatedDoc = await Video.updateOne({_id: id}, req.body)
         res.status(200).json(updatedDoc)
     } else {
-        res.setHeader('Allow', ['GET', 'DELETE'])
+        res.setHeader('Allow', ['GET', 'DELETE', 'PUT'])
         res.status(405).end(`Method ${req.method} Not Allowed`)
 
     }
