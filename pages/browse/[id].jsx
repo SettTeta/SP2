77aa1decf7ea12190e955e908dd822b1178b7124
Sett Teta <stetanonsakul@gmail.com>
2023-02-24 18:23:21 +0700

    before redo

--- pages/browse/[id].jsx
+++ pages/browse/[id].jsx
@@ -5,8 +5,8 @@ import Header from 'components/header'
 import Footer from 'components/footer'
 
 
-const url = "https://sp-2-eta.vercel.app"
-// const url = "http://localhost:3000"
+// const url = "https://sp-2-eta.vercel.app"
+const url = "http://localhost:3000"
 
 // Step 2: This component is rendered from the server (Server-Side Rendering) SSR
 export default function Video({ video }) {
@@ -29,7 +29,7 @@ export default function Video({ video }) {
             <section className="jumbotron">
                 <div className="container">
                     <br></br>
-                    <h1 className="jumbotron-heading"><u>{video.title}</u></h1>
+                    <h1 className="jumbotron-heading">{video.title}</h1>
                     <p className="lead text-muted">
                         {video.desc}
                         <br></br>
@@ -40,18 +40,21 @@ export default function Video({ video }) {
 
             <div className="album py-5 bg-light">
                 <div className="container-xxl content-row">
-                    <row>
-                        <iframe className="vr-iframe" height="235" frameborder="0" scrolling="yes" allowvr="yes" allow="vr,gyroscope,accelerometer" flex-grow="1" border="none" margin="0" padding="0"
-                            src={video.link} allowFullScreen>
-                        </iframe>
-                    </row>
 
-                    <row>
-                        <div style={{ position: 'relative', paddingBottom: '56.25%', height: '0', overflow: 'hidden' }}>
-                            <iframe className="vr-iframe" src="https://player.stornaway.io/embed/dd20538e" frameborder="0" allowvr="yes" allowfullscreen allow="vr; accelerometer; gyroscope; autoplay;"
-                                style={{ width: '100%', height: '100%', position: 'absolute', top: '0', left: '0' }}></iframe>
-                            <script src="https://studio.stornaway.io/embed/v1/player.js" defer></script></div>
-                    </row>
+                    <div style={{ position: 'relative', paddingBottom: '56.25%', height: '0', overflow: 'hidden' }}>
+                        <iframe className="vr-iframe" height="235" frameborder="0" scrolling="yes" allowvr="yes" allow="vr,gyroscope,accelerometer" flex-grow="1" border="none" margin="0" padding="0"
+                            src={video.link} style={{ width: '100%', height: '100%', position: 'absolute', top: '0', left: '0' }} allowFullScreen></iframe>
+                    </div>
+
+                    {/* <iframe className="vr-iframe" height="235" frameborder="0" scrolling="yes" allowvr="yes" allow="vr,gyroscope,accelerometer" flex-grow="1" border="none" margin="0" padding="0"
+                            src={video.link} allowFullScreen>
+                        </iframe> */}
+
+                    {/* <div style={{ position: 'relative', paddingBottom: '56.25%', height: '0', overflow: 'hidden' }}>
+                        <iframe className="vr-iframe" src={video.link} frameborder="0" allowvr="yes" allowfullscreen allow="vr; accelerometer; gyroscope; autoplay;"
+                            style={{ width: '100%', height: '100%', position: 'absolute', top: '0', left: '0' }}></iframe>
+                        <script src="https://studio.stornaway.io/embed/v1/player.js" defer></script>
+                    </div> */}
                 </div>
             </div>
             <Link href="/browse">Back</Link>
