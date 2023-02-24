69c2f2d4bedfefe8463ef4c36f59f1079ea51f56
Sett Teta <stetanonsakul@gmail.com>
2023-02-24 15:38:28 +0700

    test storn 1

--- pages/browse/[id].jsx
+++ pages/browse/[id].jsx
@@ -5,7 +5,7 @@ import Header from 'components/header'
 import Footer from 'components/footer'
 
 
- const url = "https://sp-2-eta.vercel.app"
+const url = "https://sp-2-eta.vercel.app"
 // const url = "http://localhost:3000"
 
 // Step 2: This component is rendered from the server (Server-Side Rendering) SSR
@@ -40,9 +40,14 @@ export default function Video({ video }) {
 
             <div className="album py-5 bg-light">
                 <div className="container-xxl content-row">
-                <iframe className="vr-iframe" height="235" frameborder="0" scrolling="yes" allowvr="yes" allow="vr,gyroscope,accelerometer" flex-grow="1" border="none" margin="0" padding="0"
-                    src={video.link} allowFullScreen>
-                </iframe>
+
+                    {/* <iframe className="vr-iframe" height="235" frameborder="0" scrolling="yes" allowvr="yes" allow="vr,gyroscope,accelerometer" flex-grow="1" border="none" margin="0" padding="0"
+                        src={video.link} allowFullScreen>
+                    </iframe> */}
+
+                    <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">
+                        <iframe src="https://player.stornaway.io/embed/dd20538e" frameborder="0" allowfullscreen allow="accelerometer; gyroscope;autoplay;" style="width: 100%; height: 100%; position: absolute; top: 0; left: 0;"></iframe> 
+                        <script src="https://studio.stornaway.io/embed/v1/player.js" defer></script></div>
                 </div>
             </div>
             <Link href="/browse">Back</Link>
